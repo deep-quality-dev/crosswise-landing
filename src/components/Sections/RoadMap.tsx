48e8a348ac73aa2ae214fb8147fe32d49a2f30c9
bigdreamer07 <aladin.miracle2021@gmail.com>
2022-01-10 05:54:23 -0800

    fix stage titles from same q1 2022

--- src/components/Sections/RoadMap.tsx
+++ src/components/Sections/RoadMap.tsx
@@ -2,144 +2,152 @@ import {
   Box,
   Container,
   Heading,
-  Icon,
   SimpleGrid,
   Spacer,
   Stack,
   Flex,
   Text,
   Link,
-  Button,
-  useColorModeValue,
 } from '@chakra-ui/react';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React from 'react';
 
 import { withSection } from './withSection';
 
-import ImageRocket from '@/assets/images/rocket.png';
 import ImageEarth from '@/assets/images/earth.png';
-import { IconArrowDown } from '../icons';
 
-const Feature = ({
-  title,
-  description,
-}: {
+interface StageProps {
   title: string;
-  description: string;
-}) => {
-  const [expand, setExpand] = useState(false);
+}
 
-  const handleExpand = () => {
-    setExpand(!expand);
-  };
-
-  {
-    /* <Box
-        position={'absolute'}
-        left={'0'}
-        right={'0'}
-        top={'0'}
-        bottom={'0'}
-        bg={'#25272C'}
-      /> */
-  }
-
+// 'border-image-source: linear-gradient(180deg, #04F8AD 0%, #3F81EF 50%, #8750F4 100%)'
+const Stage = ({ title }: StageProps) => {
   return (
     <Box
-      bg={useColorModeValue(
-        'transparent',
-        'linear-gradient(89.98deg, rgba(245, 247, 250, 0.12) 0.02%, rgba(255, 255, 255, 0.06) 50%, rgba(255, 255, 255, 0) 99.99%);'
-      )}
-      p={{ base: '16px' }}
-      borderRadius={'10px'}
-      boxShadow={'0 .5rem 1rem rgb(0 0 0/15%) !important'}
       position={'relative'}
+      zIndex={0}
+      borderRadius={'60px'}
+      overflow={'hidden'}
+      padding={'6px'}
+      marginY={'26px'}
     >
-      <Stack
-        justifyContent={'space-between'}
-        spacing={2}
-        direction={'row'}
-        alignItems={'center'}
+      <Box
+        position={'absolute'}
+        left={0}
+        top={0}
+        zIndex={-1}
+        width={'100%'}
+        height={'100%'}
+        backgroundImage={
+          'linear-gradient(180deg, #04F8AD 0%, #3F81EF 50%, #8750F4 100%)'
+        }
+      ></Box>
+      <Box
+        borderRadius={'60px'}
+        backgroundColor={'#1B272D'}
+        fontSize={'20px'}
+        boxShadow={'inset 1px 10px 50px rgba(0, 0, 0, 0.45)'}
+        padding={'12px 30px'}
       >
-        <Text>{title}</Text>
-        <Icon
-          as={IconArrowDown}
-          onClick={handleExpand}
-          color={useColorModeValue('gray.100', 'white')}
-        />
-      </Stack>
-      {expand && <Text pt={5}>{description}</Text>}
+        {title}
+      </Box>
     </Box>
   );
 };
 
-interface StageProps {
-  title: string
-}
-
-// 'border-image-source: linear-gradient(180deg, #04F8AD 0%, #3F81EF 50%, #8750F4 100%)'
-const Stage = ({title} : StageProps) => {
-  return (
-    <Box position={'relative'} zIndex={0} borderRadius={'60px'} overflow={'hidden'} padding={'6px'} marginY={'26px'}>
-      <Box position={'absolute'} left={0} top={0} zIndex={-1} width={'100%'} height={'100%'} backgroundImage={'linear-gradient(180deg, #04F8AD 0%, #3F81EF 50%, #8750F4 100%)'}>
-      </Box>
-      <Box borderRadius={'60px'} backgroundColor={'#1B272D'} fontSize={'20px'} boxShadow={'inset 1px 10px 50px rgba(0, 0, 0, 0.45)'} padding={'12px 30px'}>
-        {title}
-      </Box>      
-    </Box>
-  )
-}
-
 interface CircleProps {
-  description?: string
+  description?: string;
 }
-const Circle = ({description} : CircleProps) => {
+const Circle = ({ description }: CircleProps) => {
   return (
     <Box position={'relative'} width={'100%'}>
-      <Box position={'relative'} zIndex={0} borderRadius={'50%'} overflow={'hidden'} padding={'2px'} width={'22px'} height={'22px'} marginX={'auto'} marginY={'13px'}>
-        <Box position={'absolute'} left={0} top={0} zIndex={-1} width={'100%'} height={'100%'} backgroundImage={'linear-gradient(180deg, #04F8AD 0%, #3F81EF 50%, #8750F4 100%)'}>
-        </Box>
-        <Box borderRadius={'50%'} backgroundColor={'#1B272D'} fontSize={'20px'} boxShadow={'inset 1px 10px 50px rgba(0, 0, 0, 0.45)'} height={'100%'}>
-        </Box>    
+      <Box
+        position={'relative'}
+        zIndex={0}
+        borderRadius={'50%'}
+        overflow={'hidden'}
+        padding={'2px'}
+        width={'22px'}
+        height={'22px'}
+        marginX={'auto'}
+        marginY={'13px'}
+      >
+        <Box
+          position={'absolute'}
+          left={0}
+          top={0}
+          zIndex={-1}
+          width={'100%'}
+          height={'100%'}
+          backgroundImage={
+            'linear-gradient(180deg, #04F8AD 0%, #3F81EF 50%, #8750F4 100%)'
+          }
+        ></Box>
+        <Box
+          borderRadius={'50%'}
+          backgroundColor={'#1B272D'}
+          fontSize={'20px'}
+          boxShadow={'inset 1px 10px 50px rgba(0, 0, 0, 0.45)'}
+          height={'100%'}
+        ></Box>
       </Box>
-      <Box position={'absolute'} left={'calc(50% + 30px)'} top={'12px'} maxWidth={'240px'} fontSize={'16px'}>
+      <Box
+        position={'absolute'}
+        left={'calc(50% + 30px)'}
+        top={'12px'}
+        maxWidth={'240px'}
+        fontSize={'16px'}
+      >
         {description}
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 const Map = () => {
   return (
-    <Flex position={'relative'} zIndex={0} direction={'column'} alignItems={'center'} width={'100%'} paddingBottom={'40px'}>
+    <Flex
+      position={'relative'}
+      zIndex={0}
+      direction={'column'}
+      alignItems={'center'}
+      width={'100%'}
+      paddingBottom={'40px'}
+    >
       <Stage title="Q1 2022" />
-      <Circle description='Token Listing onDEX - launch on BSC' />
-      <Circle description='Smart Contract Audits' />
-      <Circle description='Crosswise API Implementation'/>
-      <Stage title="Q1 2022" />
-      <Circle description='Launchpad Release'/>
-      <Circle description='Ramp Network Integration'/>
-      <Circle description='Front-Running Deterrents'/>
-      <Circle description='Launch on Polygon'/>
-      <Stage title="Q1 2022" />
-      <Circle description='Industry Partnerships'/>
-      <Circle description='Synthetic Assets'/>
-      <Circle description='Cross-Chain Swaps'/>
-      <Circle description='Token Bridge'/>
-      <Circle description='Launch on Ethereum'/>
+      <Circle description="Token Listing onDEX - launch on BSC" />
+      <Circle description="Smart Contract Audits" />
+      <Circle description="Crosswise API Implementation" />
+      <Stage title="Q2 2022" />
+      <Circle description="Launchpad Release" />
+      <Circle description="Ramp Network Integration" />
+      <Circle description="Front-Running Deterrents" />
+      <Circle description="Launch on Polygon" />
+      <Stage title="Q3 2022" />
+      <Circle description="Industry Partnerships" />
+      <Circle description="Synthetic Assets" />
+      <Circle description="Cross-Chain Swaps" />
+      <Circle description="Token Bridge" />
+      <Circle description="Launch on Ethereum" />
 
-      <Box position={'absolute'} left={'calc(50% - 4px)'} top={0} width={'8px'} height={'100%'}
-        backgroundImage={'linear-gradient(180deg, rgba(4, 248, 173, 0) 0%, #04F8AD 19.79%, #3F81EF 45.31%, #8750F4 80.73%, rgba(135, 80, 244, 0) 100%)'}
+      <Box
+        position={'absolute'}
+        left={'calc(50% - 4px)'}
+        top={0}
+        width={'8px'}
+        height={'100%'}
+        backgroundImage={
+          'linear-gradient(180deg, rgba(4, 248, 173, 0) 0%, #04F8AD 19.79%, #3F81EF 45.31%, #8750F4 80.73%, rgba(135, 80, 244, 0) 100%)'
+        }
         zIndex={-1}
-       />
+      />
     </Flex>
-  )
-}
+  );
+};
 
 const RoadMap = () => {
   return (
-    <Box position={'relative'} >
+    <Box position={'relative'}>
       <Stack
         as={Container}
         maxW={'7xl'}
@@ -147,9 +155,7 @@ const RoadMap = () => {
         py={{ base: 24, lg: 32 }}
         spacing={{ base: 5 }}
       >
-        <Stack
-          alignItems={'center'}
-        >
+        <Stack alignItems={'center'}>
           <Heading
             as={'h3'}
             fontWeight={400}
@@ -161,19 +167,45 @@ const RoadMap = () => {
           </Heading>
           <Map />
         </Stack>
-        <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={8} pt={{ base: 10, lg: 24 }}>  
-          <Image src={ImageEarth} objectFit='contain' />
-          <Stack
-            direction={'column'}
-          >
-            <Box position={'relative'} zIndex={0} borderRadius={'60px'} overflow={'hidden'} padding={'1px'} width={'fit-content'}>
-              <Box position={'absolute'} left={0} top={0} zIndex={-1} width={'100%'} height={'100%'} backgroundImage={'linear-gradient(270deg, #3F81EF 24.48%, #04F8AD 100%)'}>
-              </Box>
-              <Box borderRadius={'60px'} backgroundColor={'#0e263e'} fontSize={'20px'} boxShadow={'inset 1px 10px 50px rgba(0, 0, 0, 0.45)'} padding={'5px 12px'} color={'#04F8AD'} fontWeight={'light'}>
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          spacing={8}
+          pt={{ base: 10, lg: 24 }}
+        >
+          <Image src={ImageEarth} objectFit="contain" />
+          <Stack direction={'column'}>
+            <Box
+              position={'relative'}
+              zIndex={0}
+              borderRadius={'60px'}
+              overflow={'hidden'}
+              padding={'1px'}
+              width={'fit-content'}
+            >
+              <Box
+                position={'absolute'}
+                left={0}
+                top={0}
+                zIndex={-1}
+                width={'100%'}
+                height={'100%'}
+                backgroundImage={
+                  'linear-gradient(270deg, #3F81EF 24.48%, #04F8AD 100%)'
+                }
+              ></Box>
+              <Box
+                borderRadius={'60px'}
+                backgroundColor={'#0e263e'}
+                fontSize={'20px'}
+                boxShadow={'inset 1px 10px 50px rgba(0, 0, 0, 0.45)'}
+                padding={'5px 12px'}
+                color={'#04F8AD'}
+                fontWeight={'light'}
+              >
                 Coming soon
-              </Box>      
+              </Box>
             </Box>
-            
+
             <Spacer />
             <Heading
               as={'h3'}
@@ -185,13 +217,12 @@ const RoadMap = () => {
             </Heading>
             <Spacer />
             <Text fontSize={'20px'} fontWeight={'300'}>
-              The Crosswise Launchpad is a protocol designed for cross-chain token pools as well as auctions, enabling our ecosystem projects to raise capital on a decentralized & interoperable environment. 
+              The Crosswise Launchpad is a protocol designed for cross-chain
+              token pools as well as auctions, enabling our ecosystem projects
+              to raise capital on a decentralized & interoperable environment.
             </Text>
             <Spacer />
-            <Link
-              style={{ textDecoration: 'none' }}
-              href={'#'}
-            >
+            <Link style={{ textDecoration: 'none' }} href={'#'}>
               <Text color={'#04F8AD'}>Learn more &#8594;</Text>
             </Link>
           </Stack>
