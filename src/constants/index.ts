3ef9f54161fef66c431bb5b8f995e4659f702cd1
deep-quality-dev <hungrywarrior081@gmail.com>
2022-01-11 23:55:31 +1000

    fix: add discord at the footer

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,6 +7,8 @@ export const TELEGRAM_LINK = 'https://t.me/crosswise';
 
 export const TWITTER_LINK = 'https://twitter.com/crosswisefi';
 
+export const DISCORD_LINK = 'https://discord.gg/4BuwxMz24J';
+
 export const MEDIUM_LINK = 'https://crosswise.medium.com/';
 
 export const GITHUB_LINK = 'https://github.com/crosswise-finance';
