56a03e8c080bfb42014d42a9b9aa760e617d5724
bigdreamer07 <aladin.miracle2021@gmail.com>
2022-01-14 01:44:50 -0800

    animations for road map and solutions

--- src/constants/animations.ts
+++ src/constants/animations.ts
@@ -29,18 +29,18 @@ const zoomInAnim = keyframes`${zoomIn}`;
 
 const flipAnim = keyframes`${flip}`;
 
-export const fadeInAnimation = `${fadeInAnim} 1s linear`;
-export const fadeInLeftAnimation = `${fadeInLeftAnim} 1s linear`;
-export const fadeInRightAnimation = `${fadeInRightAnim} 1s linear`;
-export const fadeInDownAnimation = `${fadeInDownAnim} 1s linear`;
-export const fadeInUpAnimation = `${fadeInUpAnim} 1s linear`;
-export const fadeInDownBigAnimation = `${fadeInDownBigAnim} 1s linear`;
-export const fadeInUpBigAnimation = `${fadeInUpBigAnim} 1s linear`;
+export const fadeInAnimation = `${fadeInAnim} 1.5s linear`;
+export const fadeInLeftAnimation = `${fadeInLeftAnim} 1.5s linear`;
+export const fadeInRightAnimation = `${fadeInRightAnim} 1.5s linear`;
+export const fadeInDownAnimation = `${fadeInDownAnim} 1.5s linear`;
+export const fadeInUpAnimation = `${fadeInUpAnim} 1.5s linear`;
+export const fadeInDownBigAnimation = `${fadeInDownBigAnim} 1.5s linear`;
+export const fadeInUpBigAnimation = `${fadeInUpBigAnim} 1.5s linear`;
 
 export const bounceInAnimation = `${bounceInAnim} 2s linear`;
 
 export const slideInDownAnimation = `${slideInDownAnim} 2s linear`;
 
-export const zoomInAnimation = `${zoomInAnim} 1s linear`;
+export const zoomInAnimation = `${zoomInAnim} 1.5s linear`;
 
 export const flipAnimation = `${flipAnim} 2s linear`;
